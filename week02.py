e3f686b91c261b1f1b770fe2e76218b2c05e31f2
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2025-03-17 15:40:07 +0900

    v0.3 O(log n) Guess number

--- week02.py
+++ week02.py
@@ -1,5 +1,21 @@
-#alt + shift + F10
-n = int(input("정수 입력"))
-result = 0
-result = (n * (n + 1)) // 2 #O(1)
-print(result)
+import random
+from itertools import count
+
+answer = random.randint(1,100)
+game = False
+count = 1
+for i in range(7):
+    guess = int(input("guess number : "))
+    if guess < answer:
+        print(f"your number is low, {7 - count} chance left")
+    elif guess > answer:
+        print(f"your number is high, {7 - count} chance left")
+    else:
+        game = True
+        break
+    count += 1
+if game:
+    print(f"you win!! the answer is {answer}")
+else:
+    print(f"you lose. the answer is {answer}")
+
