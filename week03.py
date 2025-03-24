a71ad11cbb0df1ea986868082ea583f3e79c0d89
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2025-03-24 14:58:10 +0900

    v0.8 Array

--- week03.py
+++ week03.py
@@ -1,7 +1,4 @@
-groups = ['HOT', 'Seventeen', 'Black pink', 'NJZ']
-# ratings = [1, 2, 4, 3, 100]
-ratings = [1, 2, 4, 3]
+cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
+cities = set(cities)
 
-group_rating = list(zip(groups, ratings))
-# zip : 리스트들의 같은 인덱스의 값을 튜플로 묶어서 반환
-print(group_rating)
+print(cities)
