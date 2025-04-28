2599cbe614f4b430985bcfea67e3bece48b39f30
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2025-04-28 15:14:12 +0900

    v3.2 Tree(BST)

--- week08.py
+++ week08.py
@@ -1,53 +1,40 @@
+def post_order(node):
+    if node:
+        post_order(node.left)
+        post_order(node.right)
+        print(node.data, end=" -> ")
+
 class TreeNode:
     def __init__(self):
         self.left = None
         self.right = None
         self.data = None
 
-def post_order(node):
-    if node:
-        post_order(node.left)
-        post_order(node.right)
-        print(node.data, end=' - ')
+if __name__ == "__main__":
+    numbers  = [10, 15, 8, 3, 9]
+    root = None
 
-def in_order(node):
-    if node:
-        in_order(node.left)
-        print(node.data, end=' - ')
-        in_order(node.right)
+    node = TreeNode()
+    node.data = numbers[0]
+    root = node
 
-def pre_order(node):
-    if node:
-        print(node.data, end=' - ')
-        pre_order(node.left)
-        pre_order(node.right)
+    # 2번째 원소 부터
+    for number in numbers[1:]:
+        node = TreeNode()
+        node.data = number
+        current = root
+        while(1):
+            if number < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right
 
 
-node1 = TreeNode()
-node1.data = 'hs'
-
-node2 = TreeNode()
-node2.data = 'sl'
-node1.left = node2
-
-node3 = TreeNode()
-node3.data = 'mb'
-node1.right = node3
-
-node4 = TreeNode()
-node4.data = 'hw'
-node2.left = node4
-
-node5 = TreeNode()
-node5.data = 'zz'
-node2.right = node5
-
-node6 = TreeNode()
-node6.data = 'sm'
-node3.left = node6
-
-post_order(node1)
-print()
-in_order(node1)
-print()
-pre_order(node1)
+print("BST 구성 완료")
+post_order(root)
